4b7b3cce94a42e65f34fb643d504a4ccef9206a1
Guilherme <guilhermegpereira26@hotmail.com>
2025-06-27 17:18:17 -0300

    Add metodo DELETE

--- src/main/java/io/github/Guimaraes131/desafio_itau_backend/controller/TransacaoController.java
+++ src/main/java/io/github/Guimaraes131/desafio_itau_backend/controller/TransacaoController.java
@@ -4,10 +4,7 @@ import io.github.Guimaraes131.desafio_itau_backend.model.Transacao;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -26,5 +23,12 @@ public class TransacaoController {
         repositorio.add(transacao);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> destroy() {
+        repositorio.removeAll(repositorio);
+
+        return ResponseEntity.ok().build();
     }
 }
