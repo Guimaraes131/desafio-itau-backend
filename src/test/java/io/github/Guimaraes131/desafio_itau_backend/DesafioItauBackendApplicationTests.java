f946ed4c844a0cac8c82811fdfbd6e46132e43eb
Guilherme <guilhermegpereira26@hotmail.com>
2025-06-27 15:00:46 -0300

    Iniciando projeto

---
+++ src/test/java/io/github/Guimaraes131/desafio_itau_backend/DesafioItauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.desafio_itau_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
